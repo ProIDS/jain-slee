68926b88fd5933e7cc99562847af8872f3cc592a
SergeyLee <sergey.povarnin@mobius-software.com>
2016-10-31 12:15:38 +0200

    Clean datasource-ejb-app.

--- container/datasource-ejb-app/datasource-ejb-bean/src/main/java/org/mobicents/slee/container/datasource/SleeDataSourceBean.java
+++ container/datasource-ejb-app/datasource-ejb-bean/src/main/java/org/mobicents/slee/container/datasource/SleeDataSourceBean.java
@@ -12,32 +12,24 @@ import javax.sql.DataSource;
 @EJB(name = "SleeDataSourceBean", beanInterface = SleeDataSourceInterface.class)
 public class SleeDataSourceBean implements SleeDataSourceInterface {
     private SleeDataSourceListener listener;
-    private Boolean locking = true;
 
     @Resource(name="java:jboss/datasources/ExampleDS")
     private DataSource liveDataSource;
 
     @PostConstruct
     private void startup() {
-        System.out.println("HELLO from SleeDataSource Singleton!");
     }
 
     @PreDestroy
     private void shutdown() {
-        System.out.println("GOODBYE from SleeDataSource Singleton!");
-
-        System.out.println("listener: "+listener);
         if (listener != null) {
             listener.shutdown();
             listener.waitForShutdownCompletion();
         }
-
-        System.out.println("GOODBYE COMPLETED from SleeDataSource Singleton!");
     }
 
     @Override
     public void setListener(SleeDataSourceListener listener) {
-        System.out.println("listener: " + listener);
         this.listener = listener;
     }
 }
