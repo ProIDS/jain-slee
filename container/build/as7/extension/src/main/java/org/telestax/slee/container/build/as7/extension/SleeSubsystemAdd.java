faeb04dd5310ed1aaa10af79e3b60b01df532bd9
SergeyLee <sergey.povarnin@mobius-software.com>
2016-10-31 12:56:23 +0200

    Added datasource EJB app as deployment (a mixed approach solution).

--- container/build/as7/extension/src/main/java/org/telestax/slee/container/build/as7/extension/SleeSubsystemAdd.java
+++ container/build/as7/extension/src/main/java/org/telestax/slee/container/build/as7/extension/SleeSubsystemAdd.java
@@ -1,14 +1,9 @@
 package org.telestax.slee.container.build.as7.extension;
 
-import java.util.List;
-
-import javax.management.MBeanServer;
-import javax.transaction.TransactionManager;
-
-import org.jboss.as.controller.AbstractBoottimeAddStepHandler;
-import org.jboss.as.controller.OperationContext;
-import org.jboss.as.controller.OperationFailedException;
-import org.jboss.as.controller.ServiceVerificationHandler;
+import org.jboss.as.controller.*;
+import org.jboss.as.controller.operations.common.Util;
+import org.jboss.as.controller.registry.ImmutableManagementResourceRegistration;
+import org.jboss.as.controller.registry.Resource;
 import org.jboss.as.jmx.MBeanServerService;
 import org.jboss.as.server.AbstractDeploymentChainStep;
 import org.jboss.as.server.DeploymentProcessorTarget;
@@ -23,20 +18,64 @@ import org.telestax.slee.container.build.as7.deployment.SleeDeploymentParseProce
 import org.telestax.slee.container.build.as7.service.SleeContainerService;
 import org.telestax.slee.container.build.as7.service.SleeServiceNames;
 
+import javax.management.MBeanServer;
+import javax.transaction.TransactionManager;
+import java.util.List;
+
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.*;
+
 class SleeSubsystemAdd extends AbstractBoottimeAddStepHandler {
 
     static final SleeSubsystemAdd INSTANCE = new SleeSubsystemAdd();
 
     private final Logger log = Logger.getLogger(SleeSubsystemAdd.class);
+    private final String deploymentsPath = "modules/system/layers/base/org/telestax/slee/container/extension/main/deployments/";
 
     private SleeSubsystemAdd() {
     }
 
     /** {@inheritDoc} */
     @Override
+    protected void populateModel(OperationContext context, ModelNode operation, Resource resource) throws OperationFailedException {
+        log.info("Populating the model: "+resource.getModel());
+        resource.getModel().setEmptyObject();
+
+        // Add a steps to install deployments (a mixed approach solution)
+        if (requiresRuntime(context)) {
+            addDeploymentStep(context, "datasource-ejb-app-deployment.ear");
+        }
+
+    }
+
+    private void addDeploymentStep(OperationContext context, String deploymentName) {
+        PathAddress deploymentAddress = PathAddress.pathAddress(PathElement.pathElement(DEPLOYMENT, deploymentName));
+        ModelNode op = Util.createOperation(ADD, deploymentAddress);
+        op.get(ENABLED).set(true);
+        op.get(PERSISTENT).set(false); // prevents writing this deployment out to standalone.xml
+
+        ModelNode contentItem = new ModelNode();
+        contentItem.get(RELATIVE_TO).set("jboss.home.dir");
+        contentItem.get(PATH).set(deploymentsPath + deploymentName);
+        contentItem.get(ARCHIVE).set(true);
+
+        op.get(CONTENT).add(contentItem);
+        log.info("op: "+op.toJSONString(false));
+
+        ImmutableManagementResourceRegistration rootResourceRegistration = context.getRootResourceRegistration();
+        OperationStepHandler handler = rootResourceRegistration.getOperationHandler(deploymentAddress, ADD);
+
+        context.addStep(op, handler, OperationContext.Stage.MODEL);
+    }
+
+    /** {@inheritDoc} */
+    @Override
     protected void populateModel(ModelNode operation, ModelNode model) throws OperationFailedException {
-        log.info("Populating the model");
-        model.setEmptyObject();
+        // We overrode the code that calls this method
+        throw new UnsupportedOperationException();
+    }
+
+    protected boolean requiresRuntimeVerification() {
+        return false;
     }
 
     /** {@inheritDoc} */
--- container/build/as7/extension/src/main/java/org/telestax/slee/container/build/as7/extension/SleeSubsystemRemove.java
+++ container/build/as7/extension/src/main/java/org/telestax/slee/container/build/as7/extension/SleeSubsystemRemove.java
@@ -1,9 +1,13 @@
 package org.telestax.slee.container.build.as7.extension;
 
-import org.jboss.as.controller.AbstractRemoveStepHandler;
-import org.jboss.as.controller.OperationContext;
-import org.jboss.as.controller.OperationFailedException;
+import org.jboss.as.controller.*;
+import org.jboss.as.controller.operations.common.Util;
+import org.jboss.as.controller.registry.ImmutableManagementResourceRegistration;
 import org.jboss.dmr.ModelNode;
+import org.jboss.logging.Logger;
+
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.DEPLOYMENT;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.REMOVE;
 
 /**
  * Handler responsible for removing the subsystem resource from the model.
@@ -15,7 +19,28 @@ class SleeSubsystemRemove extends AbstractRemoveStepHandler {
 
     static final SleeSubsystemRemove INSTANCE = new SleeSubsystemRemove();
 
+    private final Logger log = Logger.getLogger(SleeSubsystemRemove.class);
+
     private SleeSubsystemRemove() {
+    }
+
+    @Override
+    protected void performRemove(OperationContext context, ModelNode operation, ModelNode model) throws OperationFailedException {
+        // Add a steps to remove deployments (a mixed approach solution)
+        if (requiresRuntime(context)) {
+            removeDeploymentStep(context, "datasource-ejb-app-deployment.ear");
+        }
+        super.performRemove(context, operation, model);
+    }
+
+    private void removeDeploymentStep(OperationContext context, String deploymentName) {
+        PathAddress deploymentAddress = PathAddress.pathAddress(PathElement.pathElement(DEPLOYMENT, deploymentName));
+        ModelNode op = Util.createOperation(REMOVE, deploymentAddress);
+        log.info("op: "+op.toJSONString(false));
+
+        ImmutableManagementResourceRegistration rootResourceRegistration = context.getRootResourceRegistration();
+        OperationStepHandler handler = rootResourceRegistration.getOperationHandler(deploymentAddress, REMOVE);
+        context.addStep(op, handler, OperationContext.Stage.MODEL);
     }
 
     @Override
